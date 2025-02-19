75c93cdd74c7c2441d2fbe1d455a47c47e773ac8
Oghenerukevwe Esemitodje <o.esemitodje@gmail.com>
2025-02-19 13:53:22 +0100

    Create handleHover function

--- script.js
+++ script.js
@@ -1,15 +1,28 @@
-drawGrid(3);
+const grid = document.querySelector(".container");
+
+drawGrid(16);
 
 function drawGrid(size) {
-  const grid = document.querySelector(".container");
   grid.replaceChildren();
-
+  
   for (let i = 0; i < size ** 2; i++) {
     const cell = document.createElement("div");
-
+    
     cell.style.width =  `${100 / size}%`;
     grid.appendChild(cell);
   }
+
+  handleHover();
 }
+
+function handleHover() {
+  const cells = document.querySelectorAll(".container div");
+  cells.forEach((cell) => {
+    cell.addEventListener('mouseover', () => {
+      cell.style.backgroundColor = 'black';
+    });
+  });
+}
+
 
 
