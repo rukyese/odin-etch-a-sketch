e95393e11049999bd1528961e14bbe3326344061
Oghenerukevwe Esemitodje <o.esemitodje@gmail.com>
2025-02-19 16:29:26 +0100

    Add colors to cell hover

--- script.js
+++ script.js
@@ -6,11 +6,12 @@ drawGrid(16);
 
 function drawGrid(size) {
   grid.replaceChildren();
-  
+
   for (let i = 0; i < size ** 2; i++) {
     const cell = document.createElement("div");
-    
-    cell.style.width =  `${100 / size}%`;
+
+    cell.style.width = `${100 / size}%`;
+    // cell.style.opacity = '0';
     grid.appendChild(cell);
   }
 
@@ -20,8 +21,13 @@ function drawGrid(size) {
 function handleHover() {
   const cells = document.querySelectorAll(".container div");
   cells.forEach((cell) => {
-    cell.addEventListener('mouseover', () => {
-      cell.style.backgroundColor = 'black';
+    cell.addEventListener("mouseover", () => {
+      // cell.style.backgroundColor = 'black';
+
+      const r = Math.random() * 256;
+      const g = Math.random() * 256;
+      const b = Math.random() * 256;
+      cell.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
     });
   });
 }
@@ -29,9 +35,19 @@ function handleHover() {
 function handleReset() {
   const cells = document.querySelectorAll(".container div");
   cells.forEach((cell) => {
-      cell.style.backgroundColor = 'white';
+    cell.style.backgroundColor = "white";
   });
 }
 
-resetBtn.addEventListener('click', handleReset);
+function newGrid() {
+  let size = parseInt(prompt("Enter a grid size from 1 to 100"));
 
+  if (size < 1 || size > 100) return newGrid();
+  if (!size) return;
+  if (!Number.isInteger(size)) return;
+
+  drawGrid(size);
+}
+
+resetBtn.addEventListener("click", handleReset);
+newBtn.addEventListener("click", newGrid);
